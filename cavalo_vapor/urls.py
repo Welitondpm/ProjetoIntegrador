ca6e9cda8b0504e7d895eb331e6edb1a2cc8b50c
Fel9 <fel9.renan02@gmail.com>
2021-02-13 09:52:56 -0300

    modelos base completos, chats terminados

--- cavalo_vapor/urls.py
+++ cavalo_vapor/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-app_name = 'cavalo_vapor'
+app_name = "cavalo_vapor"
 urlpatterns = [
     path("", views.index, name="index"),
     path("cadastro", views.cadastro, name="cadastro"),
@@ -10,8 +10,7 @@ urlpatterns = [
     path("frete/<int:id_frete>", views.frete_individual, name="frete_individual"),
     path("fretes", views.fretes, name="fretes"),
     path("login", views.login, name="login"),
-    path("perfil/<int:id_perfil>", views.perfil_individual,
-         name="perfil_individual"),
+    path("perfil/<int:id_perfil>", views.perfil_individual, name="perfil_individual"),
     path("perfis", views.perfis, name="perfis"),
     path("suporte", views.suporte, name="suporte"),
 ]
