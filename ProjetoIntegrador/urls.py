f99d9e36a5ae81e34921f03e57a7f3a303242e54
Weliton <welitondpm2003@gmail.com>
2021-09-23 19:27:25 -0300

    Fretes, Login, Registro e Suporte

--- ProjetoIntegrador/urls.py
+++ ProjetoIntegrador/urls.py
@@ -1,18 +1,3 @@
-"""ProjetoIntegrador URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 from cavalo_vapor import views
@@ -20,4 +5,15 @@ from cavalo_vapor import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
+    path('login/', views.login, name="login"),
+    path('cadastro/', views.cadastro, name="cadastro"),
+    path('suporte/', views.suporte, name="suporte"),
+    path('fretes/', views.fretes, name="fretes"),
+    path('atividade/', views.atividade, name="atividade"),
+    path('caminhoes/', views.caminhoes, name="caminhoes"),
+    path('chats/', views.chats, name="chats"),
+    path('mapa/', views.mapa, name="mapa"),
+    path('network/', views.network, name="network"),
+    path('perfis/', views.perfis, name="perfis"),
+    path('usuario/', views.usuario, name="usuario"),
 ]
--- cavalo_vapor/views.py
+++ cavalo_vapor/views.py
@@ -1,5 +1,38 @@
 from django.shortcuts import render
 
+
 def index(request):
     return render(request, "index.html")
 
+def login(request):
+    return render(request, "login.html")
+
+def cadastro(request):
+    return render(request, "cadastro.html")
+
+def suporte(request):
+    return render(request, "suporte.html")
+
+def fretes(request):
+    return render(request, "fretes.html")
+
+def atividade(request):
+    return render(request, "atividade.html")
+
+def caminhoes(request):
+    return render(request, "caminhoes.html")
+
+def chats(request):
+    return render(request, "chats.html")
+
+def mapa(request):
+    return render(request, "mapa.html")
+
+def network(request):
+    return render(request, "network.html")
+
+def perfis(request):
+    return render(request, "perfis.html")
+
+def usuario(request):
+    return render(request, "usuario.html")
